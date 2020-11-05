86c051e491617f7b373865701abf649a8624673b
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2020-11-05 23:10:29 +0530

    feat: Initial commit

---
+++ Assets/Adrenak.Vboard/Runtime/VirtualKey.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Adrenak.Vboard {
+    [RequireComponent(typeof(Button))]
+    public class VirtualKey : MonoBehaviour {
+        VirtualKeyboard board;
+
+        public void Start() {
+            board = GetComponentInParent<VirtualKeyboard>();
+            GetComponent<Button>().onClick.AddListener(() => {
+                board.Append(gameObject.name);
+            });
+        }
+    }
+}
